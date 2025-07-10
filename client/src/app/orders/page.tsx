d141e4fa0ed8308262f0b10c134a00084887b7a4
h3nrzi <rezaeig22@gmail.com>
2025-07-10 14:13:00 +0330

    refactor(client): improve order display logic to conditionally render 'no orders found' message

--- client/src/app/orders/page.tsx
+++ client/src/app/orders/page.tsx
@@ -3,26 +3,28 @@ import { getCurrentUserOrders } from "@/lib/api/orders-api";
 const OrdersPage = async () => {
 	const { data: orders } = await getCurrentUserOrders();
 
-	if (!orders || orders.length === 0) return <div>سفارشی یافت نشد</div>;
-
 	return (
 		<div className="m-5">
 			<h1>سفارش های من</h1>
 			<ul className="mt-5">
-				{orders.map((order) => (
-					<li key={order.id} className="d-flex align-items-center">
-						<a href={`/orders/${order.id}`}>{order.id}</a>
-						{order.status === "cancelled" && (
-							<span className="badge badge-danger mr-2">لغو شده</span>
-						)}
-						{order.status === "created" && (
-							<span className="badge badge-info mr-2">درحال انتظار</span>
-						)}
-						{order.status === "complete" && (
-							<span className="badge badge-success mr-3">تکمیل شده</span>
-						)}
-					</li>
-				))}
+				{orders && orders.length > 0 ? (
+					orders.map((order) => (
+						<li key={order.id} className="d-flex align-items-center">
+							<a href={`/orders/${order.id}`}>{order.id}</a>
+							{order.status === "cancelled" && (
+								<span className="badge badge-danger mr-2">لغو شده</span>
+							)}
+							{order.status === "created" && (
+								<span className="badge badge-info mr-2">درحال انتظار</span>
+							)}
+							{order.status === "complete" && (
+								<span className="badge badge-success mr-3">تکمیل شده</span>
+							)}
+						</li>
+					))
+				) : (
+					<div>سفارشی یافت نشد</div>
+				)}
 			</ul>
 		</div>
 	);
